faa7a489e4dc7f161237193046e484046e5c3511
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-02-11 15:29:22 +0600

    common function

--- js/banking.js
+++ js/banking.js
@@ -1,43 +1,36 @@
+function getInputValue(inputId){
+    const input = document.getElementById(inputId)
+    const amountText = input.value
+    const amount = parseFloat(amountText)
+// clear value
+    input.value = ''
+    return amount
+
+}
+
 document.getElementById('deposit-button').addEventListener('click', function(){
-    //get The Amount deposited
-    const depositAmount = document.getElementById('deposit-input').value
-    
-    // console.log(depositAmount);
+    const depositAmount = getInputValue('deposit-input')
 
     const depositCurrent = document.getElementById('deposit-total')
-
-    console.log(depositCurrent.innerText);
     
-    depositCurrent.innerText = parseFloat(depositAmount) + parseFloat(depositCurrent.innerText)
-    document.getElementById('deposit-input').value = ''
+    depositCurrent.innerText = depositAmount + parseFloat(depositCurrent.innerText)
     // balance update
 
     const balanceTotal =parseFloat(document.getElementById('balance-total').innerText)
     const newBalance = balanceTotal + parseFloat(depositAmount)
     document.getElementById('balance-total').innerText = newBalance
 
-
-    
-
-
-
 })
 
 // withdraw
 
 document.getElementById('withdraw-button').addEventListener('click', function(){
-    const withdrawAmount = parseFloat(document.getElementById('withdraw-input').value)
-
-    const withdrawCurrent = parseFloat(document.getElementById('withdraw-total').innerText)
-
-    console.log(withdrawCurrent);
     
+    const withdrawAmount = getInputValue('withdraw-input')
+
+    const withdrawCurrent = parseFloat(document.getElementById('withdraw-total').innerText)   
     document.getElementById('withdraw-total').innerText = withdrawAmount + withdrawCurrent
-
-    //clear
-    document.getElementById('withdraw-input').value = ''
     // balance update
-
     const balanceTotal =parseFloat(document.getElementById('balance-total').innerText)
     const newBalance = balanceTotal - withdrawAmount
     document.getElementById('balance-total').innerText = newBalance
